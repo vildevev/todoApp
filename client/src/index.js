b80cb72a521a68818305244c84bc97d945dbbbad
“Vilde <vilde@vevatne.no>
2017-08-12 14:01:53 -0700

    Create app component in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			loggedIn: false
+		};
+		this.onClickHandler = this.onClickHandler.bind(this);
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
