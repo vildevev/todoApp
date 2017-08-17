727405cb456c252f108482e95bb70cd226f8b2b3
“Vilde <vilde@vevatne.no>
2017-08-16 21:49:58 -0700

    Fix development sendfile function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express'),
+  path = require('path'),
   bodyParser = require('body-parser'),
   passport = require('passport'),
   cookieSession = require('cookie-session');
@@ -11,13 +12,12 @@ const auth = require('./routes/auth');
 
 const app = express();
 
-app.enable('trust proxy');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   cookieSession({
-    keys: ['blah blah'],
-    maxAge: 30 * 24 * 60 * 60 * 1000
+    keys: [process.env.COOKIE_KEY || 'secret'],
+    maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
   })
 );
 
@@ -29,17 +29,12 @@ app.use('/auth', auth);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, err => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`Successfully listening on port ${PORT}`);
-  }
+app.listen(process.env.PORT || 5000, err => {
+  if (err) return console.log(err);
+  console.log(`Successfully listening for network requests`);
 });
